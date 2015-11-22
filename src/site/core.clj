1c39bc78735a826dc5ce256c44bc004aae610b22
Fellipe Brito <fellipe@devlandia.net>
2015-11-22 06:18:08 +0000

    Adds HTTP server abstraction using Ring

--- src/site/core.clj
+++ src/site/core.clj
@@ -1,10 +1,16 @@
 (ns site.core
   (:require [compojure.core :refer :all]
+            [ring.middleware.defaults :refer :all]
             [org.httpkit.server :refer [run-server]])) ; httpkit is a server
 
 (defroutes myapp
   (GET "/" [] "Hello World")
-  (GET "/hello/:name" [name] (str "Hello " name)))
+  (GET "/hello/:name" [name] (str "Hello " name))
+
+  (GET "/posts" req
+    (let [title (get (:params req) :title)
+          author (get (:params req) :author)]
+      (str "Title: " title ", Author: " author))))
 
 (defn -main []
-  (run-server myapp {:port 5000}))
+  (run-server (wrap-defaults myapp site-defaults) {:port 5000}))
